d19aad7acfc0f20732b507c29643692c7ff65853
Steve <zjplove3618739792@gmail.com>
2024-05-07 19:05:09 -0400

    changed

---
+++ script.js
@@ -0,0 +1,30 @@
+/*
+Exercise 1: maxOfTwoNumbers()
+
+In this exercise, create a function named maxOfTwoNumbers. It should take two numbers as inputs and return the larger number. If they're equal, return either one.
+
+Exercise 1 has been completed for you:
+*/
+
+const maxOfTwoNumbers = (x, y) => {
+    if (x >= y) {
+      return x;
+    } else {
+      return y;
+    }
+  }
+  
+//   console.log('Exercise 1 Result:', maxOfTwoNumbers(3, 9)); 
+
+const isAdult = function(age) {
+    if (age >=18) {
+        return 'Adult'
+    }else {
+        return 'Minor'
+    }
+    
+}
+
+
+console.log('Exercise 2 Result:', isAdult(21));
+  
