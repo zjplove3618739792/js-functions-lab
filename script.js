3b7054843f1adaf6527f8e8f2a6ba592f27a2836
Steve <zjplove3618739792@gmail.com>
2024-05-07 21:02:32 -0400

    Test

--- script.js
+++ script.js
@@ -1,10 +1,4 @@
-/*
-Exercise 1: maxOfTwoNumbers()
 
-In this exercise, create a function named maxOfTwoNumbers. It should take two numbers as inputs and return the larger number. If they're equal, return either one.
-
-Exercise 1 has been completed for you:
-*/
 
 const maxOfTwoNumbers = (x, y) => {
     if (x >= y) {
@@ -13,18 +7,77 @@ const maxOfTwoNumbers = (x, y) => {
       return y;
     }
   }
-  
-//   console.log('Exercise 1 Result:', maxOfTwoNumbers(3, 9)); 
+console.log('Exercise 1 Result:', maxOfTwoNumbers(3, 9)); 
+
+
+
 
 const isAdult = function(age) {
     if (age >=18) {
         return 'Adult'
     }else {
         return 'Minor'
-    }
-    
+    }   
+}
+console.log('Exercise 2 Result:', isAdult(21));
+
+const isCharAVowel= (character) => {
+  if (character === 'a' || character === 'e' || character === 'i' || character === 'o' || character === 'u') {
+    return true
+  } else {
+    return false
+  }
+}
+console.log('Exercise 3 Result:', isCharAVowel('a'));
+
+const generateEmail = (username, domain) => {
+  return `${username}@${domain}`
 }
 
 
-console.log('Exercise 2 Result:', isAdult(21));
-  
+console.log('Exercise 4 Result:', generateEmail("johnsmith", "example.com"));
+
+
+const greetUser = (Username, time) => {
+  return `Good ${time}, ${Username}!`
+}
+console.log('Exercise 5 Result:', greetUser("Sam", "morning"));
+
+
+const reverseString = () => {
+
+}
+console.log('Exercise 6 Result:', reverseString("rockstar"));
+
+
+// No.7 keep getting errors. Don't know how to fix it
+
+// const checkPalindrome = (string) => {
+//  for (let i = 0; i < string.length / 2; i++) {
+//     if (string[i] !== string[string.length - 1 - i]) {
+//       return false {
+//     else {
+//       return true
+//     }
+// }
+
+// // console.log('Exercise 7 Result:', checkPalindrome("radar"));
+
+
+
+
+
+const maxOfThree = (numb_1,numb_2,numb_3) => {
+  if (numb_1>numb_2 && numb_1>numb_3) {
+    return `${numb_1}`
+  } else if (numb_2>numb_1 && numb_2>numb_3) {
+    return `${numb_2}`
+  } else{
+    return `${numb_3}`
+  }
+
+}
+console.log('Exercise 8 Result:', maxOfThree(5, 10, 8));
+
+
+const calculateTip()
